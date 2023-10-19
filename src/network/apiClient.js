68efeb666547f866ee0d1a33c55a7682227b86ae
coder_biceps <manugupta619@gmail.com>
2023-10-19 17:13:06 +0530

    feat: Intergate axios and create axios client

---
+++ src/network/apiClient.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const response = (response) => {
+  return response;
+};
+
+const error = (error) => {
+  let res = error.response;
+  //FILTER FOR PARTICULAR STATUS CODE
+  if (res.status === 401) {
+    return Promise.reject(error);
+  }
+  console.error(`Looks like there was a problem. Status Code: ` + res.status);
+  return Promise.reject(error);
+};
+
+const userResourceClient = axios.create({
+  baseURL: `https://jsonplaceholder.typicode.com`,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+const geospatialClient = axios.create({
+  baseURL: `https://jsonplaceholder.typicode.com`,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+userResourceClient.interceptors.response.use(response, error);
+geospatialClient.interceptors.response.use(response, error);
+
+export { userClient, geospatialClient };
